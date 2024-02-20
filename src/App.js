a4ba7c3fd656adc01de1f82a27ac2de429e14a14
jaiminiprajapati <jaiminihp397@gmail.com>
2024-02-20 12:36:54 +0000

    React app created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {useEffect} from "react";
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=71cc14e3';
+
+const App = () => {
+    return (
+        <h1>App</h1>
+    );
+}
+
+export default App;
